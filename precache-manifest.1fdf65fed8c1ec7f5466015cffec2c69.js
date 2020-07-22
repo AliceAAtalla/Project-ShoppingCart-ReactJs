5c5f6f80343731870ba592eb8f26e68b2471ba51
AliceAAtalla <aliceatalla36@gmail.com>
2020-07-22 10:15:23 -0300

    Updates

---
+++ precache-manifest.1fdf65fed8c1ec7f5466015cffec2c69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904b56174dcbe7feaeb8ff866efc658e",
+    "url": "/Project_ShoppingCart_ReactJs/index.html"
+  },
+  {
+    "revision": "c640280312e81157dc54",
+    "url": "/Project_ShoppingCart_ReactJs/static/css/main.b9a1a32f.chunk.css"
+  },
+  {
+    "revision": "73621b0674602f02ac58",
+    "url": "/Project_ShoppingCart_ReactJs/static/js/2.9fd2d02e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project_ShoppingCart_ReactJs/static/js/2.9fd2d02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c640280312e81157dc54",
+    "url": "/Project_ShoppingCart_ReactJs/static/js/main.39fc3eee.chunk.js"
+  },
+  {
+    "revision": "77a2d66b94a10166ab98",
+    "url": "/Project_ShoppingCart_ReactJs/static/js/runtime-main.beb41a18.js"
+  },
+  {
+    "revision": "e3c0677d84bbba62d91de480f9d2f9aa",
+    "url": "/Project_ShoppingCart_ReactJs/static/media/Logo.e3c0677d.png"
+  }
+]);
